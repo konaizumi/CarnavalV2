7bad3ec5e107d1873de51651fbbf940b75b5d93b
Cristian <cristian@localhost.localdomain>
2014-06-03 02:16:43 -0500

    Finalizado script de la sección de galería. Cambio de nombre algunas imagenes.

---
+++ assets/js/galeria.js
@@ -0,0 +1,34 @@
+
+var container = document.getElementById('showImg');
+
+var elements = document.getElementsByClassName('galeria');
+
+var _close = document.getElementById('close');
+
+_close.onclick = function (e){
+	console.log(e, e.preventDefault);
+	e.preventDefault();
+	hidePop();
+}
+
+console.log(elements, _close, container);
+
+for(var e in elements){
+
+	elements[e].onclick = function(e){
+		el = e.target;
+		console.log(el);
+
+		container.getElementsByTagName('img')[0].src = el.src;
+		showPop();
+	}
+
+}
+
+function showPop () {
+	container.style.display = 'block';
+}
+
+function hidePop () {
+	container.style.display = 'none';
+}
